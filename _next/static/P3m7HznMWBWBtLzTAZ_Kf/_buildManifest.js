2e85a195a9e6ff1dddfbb06bd5355c3e537eee8a
Karlo Prkacin <karlo@init.hr>
2022-01-01 20:31:14 +0100

    t

---
+++ _next/static/P3m7HznMWBWBtLzTAZ_Kf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/654-19ca0573aa944a9b.js","static/chunks/pages/index-f67ff1c5d3b456f0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
